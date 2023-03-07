c1c1e8898e4c6fde8568a60929b5cbf0e6ec3d0a
Your Name <leonardo_foz@hotmail.com>
2023-03-07 11:25:11 -0300

    unidade 5 - set up azure.py, settings.py

--- starter/project/settings.py
+++ starter/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+if 'WEBSITE_HOSTNAME' in os.environ: # Running on Azure
+    from .azure import *
